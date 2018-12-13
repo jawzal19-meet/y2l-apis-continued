d17b43ddfb498f77d74a9abefcd688b005e237a2
aboud20-meet <aboud20@meet.mit.edu>
2018-12-13 18:45:05 +0200

    git add .git add .git add .

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
 from flask import Flask, render_template, request
 app = Flask(__name__)
-
+import requests,json
 @app.route('/')
 def home():
     return render_template('home.html')
 
 @app.route('/study_image', methods = ['POST'])
 def study_image():
-    
+    headers = {'Authorization': 'Key 62c59b4eb52547238acd'}
     image_url = request.form['url-input']
+    api_url = "https://api.clarifai.com/v2/models/aaa03c23b3724a16a56b629203edc62c/outputs"
+
     # At this point you have the image_url value from the front end
     # Your job now is to send this information to the Clarifai API
     # and read the result, make sure that you read and understand the
@@ -16,6 +18,18 @@ def study_image():
 
     # YOUR CODE HERE!
     
+    data ={"inputs": [
+      {
+        "data": {
+          "image": {
+            "url": "image_url"
+          }
+        }
+      }
+    ]}
+
+    response = requests.post(api_url, headers=headers, data=json.dumps(data))
+
     return render_template('home.html', results="No results yet :(")
 
 if __name__ == '__main__':
